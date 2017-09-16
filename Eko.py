7a518669eba11d52da16b415a9dc4c44101dde2e
Aditya Chawla <chawlaaditya8@gmail.com>
2017-09-16 13:37:27 +0530

    dynamic pie chart

--- Eko.py
+++ Eko.py
@@ -12,13 +12,12 @@ class Eko(Core,object):
 
     def init(self):
         super(Eko, self).init()
-        # print("1")
-        dictTemp1 = {}
-        dictTemp1["unUsed"] = 100
-        self.nitrogenCycle = Abiotics.NitrogenCycle(dictTemp1)
         dictTemp2 = {}
         dictTemp2["unUsed"] = 100
         self.waterCycle = Abiotics.WaterCycle(dictTemp2)
+        dictTemp1 = {}
+        dictTemp1["unUsed"] = 100
+        self.nitrogenCycle = Abiotics.NitrogenCycle(dictTemp1)
         dictTemp3 = {}
         dictTemp3["unUsed"] = 100
         self.carbonCycle = Abiotics.CarbonCycle(dictTemp3)
@@ -35,10 +34,28 @@ class Eko(Core,object):
         Core._running = False
 
     def makePie(self, labels, sizes, explode, chartName):
-        colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+
+        self.current_sizes = sizes
+        self.current_explode = explode
+        self.chartName = chartName
+        self.current_labels = labels
+        self.colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+
+        self.fig, self.axarr = plt.subplots(2)
+        plt.subplots()
+        plt.subplots_adjust(left=0.25, bottom=0.25)
+        
         # Plot
-        plt.pies = plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        plt.axis([0, 1, -10, 10])
+        self.axarr[0].pie(sizes, explode=explode, labels=labels, colors=self.colors,
                 autopct='%1.1f%%', shadow=True, startangle=140)
+        self.x = sizes
+        self.axarr[0].set_position([0.25,0.3,.5,.5])
+
+        self.axarr[1].set_position([0.1, 0.15, 0.8, 0.03])
+        self.risk = Slider(self.axarr[1], 'in pond', 0.0, sizes[0]+sizes[1], valinit=self.x[0])
+        self.risk.on_changed(self.updatePie)
+        # axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
         plt.axis('equal')
         plt.title(chartName)
         plt.show()
@@ -60,9 +77,13 @@ class Eko(Core,object):
 # print(self.nitrogenCycle.dict)
 
     def updatePie(self, val):
-        plt.clf()
-        risk = Slider(self.axarr, 'Risk', 0, 100.0, val)
-        plt.show()
+        self.axarr[0].clear()
+        consistant_sum = self.current_sizes[0] + self.current_sizes[1]
+        self.current_sizes[0] = val
+        self.current_sizes[1] = consistant_sum - val
+        self.axarr[0].pie(self.current_sizes, explode=self.current_explode, labels=self.current_labels, colors=self.colors,
+                autopct='%1.1f%%', shadow=True, startangle=140)
+        self.fig.canvas.draw_idle()
        # self.pies.clear()
 
 eko = Eko()
