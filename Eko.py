1cf181c288b0f7c9a804845e8016a2c86537dce0
Aditya Chawla <chawlaaditya8@gmail.com>
2017-10-14 14:57:31 +0530

    display removed from Eko.py

--- Eko.py
+++ Eko.py
@@ -32,72 +32,10 @@ class Eko(Core, object):
     def stop(self):
         Core._running = False
 
-    def makePie(self, labels, sizes, explode, chartName):
-
-        self.current_sizes = sizes
-        self.current_explode = explode
-        self.chartName = chartName
-        self.current_labels = labels
-        self.colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
-
-        self.fig, self.axarr = plt.subplots(3)
-        plt.subplots_adjust(left=0.25, bottom=0.25)
-        
-        # Plot
-       # plt.title(chartName)
-        plt.axis([0, 1, -10, 10])
-        self.axarr[0].pie(sizes, explode=explode, labels=labels, colors=self.colors,
-                autopct='%1.1f%%', shadow=True, startangle=140)
-        self.x = sizes
-        self.axarr[0].set_position([0.25, 0.3, 0.5, 0.5])
-        self.axarr[2].set_position([0, 0, 0, 0])
-
-        self.axarr[1].set_position([0.1, 0.15, 0.8, 0.03])
-        self.risk = Slider(self.axarr[1], labels[0], 0.0, sizes[0]+sizes[1], valinit=self.x[0])
-        self.risk.on_changed(self.updatePie)
-        # axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
-        # self.make_picker(self.fig, self.x)
-        plt.axis('equal')
-        plt.show()
-
     def pond(self):
         self.waterCycle.showDict()
-        self.makePie(self.waterCycle.returnLabels(), self.waterCycle.returnValues(), self.waterCycle.returnExplode()
-                     , "Water Cycle")
-
         self.nitrogenCycle.showDict()
-        self.makePie(self.nitrogenCycle.returnLabels(), self.nitrogenCycle.returnValues(),
-                     self.nitrogenCycle.returnExplode(), "Nitrogen Cycle")
-
         self.carbonCycle.showDict()
-        self.makePie(self.carbonCycle.returnLabels(), self.carbonCycle.returnValues(),
-                      self.carbonCycle.returnExplode(), "Carbon Cycle")
-
-    def make_picker(self, fig, wedges):
-
-        def onclick(event):
-            wedge = event.artist
-            label = wedge.get_label()
-            print label
-
-        # Make wedges selectable
-        for wedge in wedges:
-            wedge.set_picker(True)
-
-        fig.canvas.mpl_connect('pick_event', onclick)
-
-
-# print(self.nitrogenCycle.dict)
-
-    def updatePie(self, val):
-        self.axarr[0].clear()
-        consistant_sum = self.current_sizes[0] + self.current_sizes[1]
-        self.current_sizes[0] = val
-        self.current_sizes[1] = consistant_sum - val
-        self.axarr[0].pie(self.current_sizes, explode=self.current_explode, labels=self.current_labels,
-                          colors=self.colors, autopct='%1.1f%%', shadow=True, startangle=140)
-        self.fig.canvas.draw_idle()
-       # self.pies.clear()
 
 eko = Eko()
 eko.run()
